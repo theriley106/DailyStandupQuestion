64a811741cc051bbf998aa2512f7039205a2083a
theriley106 <christopherlambert106@gmail.com>
2020-04-02 00:28:44 -0400

    Updated scrape questions

--- scrapeQuestions.py
+++ scrapeQuestions.py
@@ -2,6 +2,7 @@ import requests
 import bs4
 import json
 import os
+import re
 from unidecode import unidecode
 # Fixes the text parsing problem from the museumhack website
 
@@ -14,14 +15,14 @@ def update_file(jsonFile):
 	else:
 		currentQuestions = json.load(open(jsonFile))
 
-	currentQuestions['questions'] = set(currentQuestions['data'])
+	currentQuestions['questions'] = set(currentQuestions['questions'])
 	# This is a hacky way of making dup lookup constant time...
 
 	for val in QUESTIONS:
-		if val not in currentQuestions['data']:
-			currentQuestions['data'].add(val)
+		if val not in currentQuestions['questions']:
+			currentQuestions['questions'].add(val)
 
-	currentQuestions['questions'] = list(currentQuestions['data'])
+	currentQuestions['questions'] = list(currentQuestions['questions'])
 	# make it a list again...
 
 	with open(jsonFile, 'w') as fp:
@@ -35,11 +36,38 @@ def get_site(url):
 def get_bs4(url):
 	return bs4.BeautifulSoup(get_site(url), 'lxml')
 
+def startersWorld(url):
+	questions = []
+	page = get_bs4(url)
+	for val in page.select("#genesis-content p"):
+		text = val.getText()
+		if re.match("\d+\.\s+", text):
+			questions.append(unidecode(text.partition(" ")[2]))
+	return questions
+
+def defaultScraper(url, cssSelector):
+	return [unidecode(val.getText()) for val in get_bs4(url).select(cssSelector)]
+
+# This is a key value pair of url and custom function to parse them
+CUSTOM_SCRAPING_METHOD = {
+	"https://conversationstartersworld.com/icebreaker-questions/": startersWorld,
+}
+
+
+# This is a key value pair of the url and CSS selector to parse them
+URL_CSS_SELECTOR_MAP = {
+	"https://museumhack.com/list-icebreakers-questions/": ".text-h-2 ol li",
+	"https://improb.com/icebreaker-questions-adults/": "li strong",
+}
+
 
 if __name__ == '__main__':
-	a = get_bs4("https://museumhack.com/list-icebreakers-questions/")
-	for val in a.select(".text-h-2 ol li"):
-		question = unidecode(val.getText())
-		QUESTIONS.append(question)
+	for url, function in CUSTOM_SCRAPING_METHOD.iteritems():
+		for question in function(url):
+			QUESTIONS.append(question)
+
+	for url, selector in URL_CSS_SELECTOR_MAP.iteritems():
+		for question in defaultScraper(url, selector):
+			QUESTIONS.append(question)
 
 	update_file("file.json")
