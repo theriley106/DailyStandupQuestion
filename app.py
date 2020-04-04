51c39e964f63a226ebcf589fca56c8a5c99e9ef8
theriley106 <christopherlambert106@gmail.com>
2020-04-04 16:04:36 -0400

    Updated api to fix capitilization issue

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def index():
 
 @app.route('/new', methods=['GET'])
 def newQuestion():
-	return jsonify({"question": random.choice(QUESTIONS)})
+	return jsonify({"question": random.choice(QUESTIONS).strip().title()})
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=5000)
