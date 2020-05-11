af827850e4cd817b950444ae0b78e32683e89f9a
theriley106 <christopherlambert106@gmail.com>
2020-05-11 01:36:02 -0400

    Added questions from gpt-2

--- app.py
+++ app.py
@@ -7,14 +7,24 @@ QUESTION_FILE = "questions.json"
 
 QUESTIONS = json.load(open(QUESTION_FILE))["questions"]
 
+GENERATED_QUESTIONS = open("static/generatedQuestions.txt").read().split("\n")
+
 
 @app.route('/', methods=['GET'])
 def index():
 	return render_template("index.html")
 
+@app.route('/generated', methods=['GET'])
+def generated():
+	return render_template("generatedQuestions.html")
+
 @app.route('/new', methods=['GET'])
 def newQuestion():
 	return jsonify({"question": random.choice(QUESTIONS).strip().lower().capitalize()})
+
+@app.route('/generatedQuestions', methods=['GET'])
+def newGeneratedQuestion():
+	return jsonify({"question": random.choice(GENERATED_QUESTIONS).strip().lower().capitalize()})
 
 if __name__ == '__main__':
 	app.run(host='127.0.0.1', port=5000)
